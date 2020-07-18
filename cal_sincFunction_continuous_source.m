3161adc70d3e69013e99ece31e410c83cbadfadb
CuiaCuiSha <32769735+CuiaCuiSha@users.noreply.github.com>
2020-07-18 22:59:06 +0800

    0718
    finish cal_costfunc_field
    但是导入和计算都是手动设置的，太笨；
    后续要增加索引。

--- cal_sincFunction_continuous_source.m
+++ cal_sincFunction_continuous_source.m
@@ -1,3 +1,16 @@
+%----------------------------------------------%
+%   TR painting中
+%   为了验证点源到连续源，sinc分布和的过渡
+%   有Amplitude和复数形式的
+%   复数形式尚未完成
+%
+%
+%
+%   Date:2020.July.10
+%   Author:cschen
+%----------------------------------------------%
+
+
 clear;clc;close all
 
 x_real=-100:0.02:100;
@@ -8,12 +21,12 @@ y_imag=sinc(x_imag);
 
 for ii=1:1:200
     % 实部
-    y_real=y_real+sinc(x_real-ii*0.5+ii*0.3);
+    y_real=y_real+sinc(x_real+ii*0.3);
     subplot(2,2,1)
     plot(x_real,y_real);
     axis([-50,50,-2,5]);
     % 虚部
-    y_imag=y_imag+sinc(x_imag-ii*0.5+1*ii);
+    y_imag=y_imag+sinc(x_imag+1*ii);
     subplot(2,2,2)
     plot(x_imag,y_imag)
     axis([-50,50,-2,5]);
